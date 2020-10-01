1f493cdc43972bce3094c0f0871fa3178d4efa18
xgschwi <xgschwi@bgsu.edu>
2020-10-01 00:03:47 -0400

    Scorecard header file mostly complete, comments and possibly more functions or variables to come

--- YahtzeeGame/yahtzee.h
+++ YahtzeeGame/yahtzee.h
@@ -19,4 +19,31 @@ public:
 	int *rollDice();
 };
 
+class ScoreCard {
+private:
+	int score;
+	
+public:
+	ScoreCard();
+	~ScoreCard();
+
+	void availableScores(int* arr);
+	void chooseScoreType(char command);
+
+	int ace(int num);
+	int two(int num);
+	int three(int num);
+	int four(int num);
+	int five(int num);
+	int six(int num);
+
+	int threeKind(int* vals);
+	int fourKind(int* vals);
+	int fullHouse();
+	int small();
+	int large();
+	int yahtzee();
+	int chance(int* vals);
+
+};
 #endif
