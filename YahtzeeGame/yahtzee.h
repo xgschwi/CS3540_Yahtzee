c25f803cafb45a1c8bed82cfc3ccf7d8f7b806d8
Nicholas McBride <nrmcbri@bgsu.edu>
2020-10-06 01:45:27 +0000

    Add dice functionality to the header.

--- YahtzeeGame/yahtzee.h
+++ YahtzeeGame/yahtzee.h
@@ -10,13 +10,61 @@ public:
 	int rollDie();
 };
 
-class Dice{
+class Dice {
 private:
-	int values[5]; // Holds the face values of all the die
-	Die **dice; // pointer to die objects
+	//int values[5]; // Holds the face values of all the die
+	int values[5] = { 1, 1, 1, 1 , 1 };
+	Die** dice; // pointer to die objects
 public:
 	Dice();
-	int *rollDice();
+	int* rollDice();
 };
 
+class ScoreCard {
+private:
+	int score;
+
+	bool aceb;
+	bool twob;
+	bool threeb;
+	bool fourb;
+	bool fiveb;
+	bool sixb;
+
+	bool threeKb;
+	bool fourKb;
+	bool fullHb;
+	bool smallb;
+	bool largeb;
+	bool yahtzeeb;
+	bool chanceb;
+public:
+	ScoreCard();
+	~ScoreCard();
+
+	void chooseScores(int* vals);
+
+	int aceF(int num);
+	int twoF(int num);
+	int threeF(int num);
+	int fourF(int num);
+	int fiveF(int num);
+	int sixF(int num);
+
+	int threeKindF(int* vals);
+	int fourKindF(int* vals);
+	int fullHouseF();
+	int smallF();
+	int largeF();
+	int yahtzeeF();
+	int chanceF(int* vals);
+
+};
+
+class Game :public Dice, public ScoreCard {
+private:
+	ScoreCard scoreCard;
+public:
+	int play(); //begins the game
+};
 #endif
