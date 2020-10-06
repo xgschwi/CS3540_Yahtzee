3ac0d57718204b5f946a5d9a0e88fd1742a8aa20
Nicholas McBride <nrmcbri@bgsu.edu>
2020-10-06 01:48:16 +0000

    Fix yahtzeeDriver.cpp

--- YahtzeeGame/yahtzeeDriver.cpp
+++ YahtzeeGame/yahtzeeDriver.cpp
@@ -4,12 +4,31 @@
 
 using namespace std;
 
+void start() {
+	int answer = 0;
+	cout << "Press 1 to play the game, or press 2 to learn how this works.";
+	cin >> answer;
+	if (answer == 1) {
+		Game game;
+		game.play();
+	}
+	else if (answer == 2) {
+		cout << "This is like a game of Yahtzee , but with a twist: you don't keep individal dice to keep as you reroll, but you instead keep scores instead! This goes on until all the scoring catagories are full. Now, we'll ask again:" << endl;
+		start();
+	}
+	else {
+		cout << "Sorry, that's not a valid number: please try again." << endl;
+		start();
+	}
+
+}
+
 int main() {
 	unsigned int seed = time(0); // Or use NULL
 	srand(seed); // Random numbers are seeded once for the rollDie function (srand(time(0)))
-
+	start();
 	Dice dice;
-	int *values; // Holds array of dice face values
+	int* values; // Holds array of dice face values
 	values = dice.rollDice(); // Dont forget to re-roll
 
 	for (int i = 0; i < 5; i++) cout << values[i]; // Demos the roll dice function
