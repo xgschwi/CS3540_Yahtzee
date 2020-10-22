c291022a4f6f7516cdef47422ee6b478074d8235
Nicholas McBride <nrmcbri@bgsu.edu>
2020-10-22 17:09:30 +0000

    Update tests.cpp

--- YahtzeeGame/tests/tests.cpp
+++ YahtzeeGame/tests/tests.cpp
@@ -19,3 +19,36 @@ TEST_CASE("Dice can be rolled", "[dice]") {
 
     REQUIRE(sum > 0);
 }
+
+TEST_CASE("Score starts at 0", "[score]"){
+    Scorecard score;
+    REQUIRE(score.Scorecard == 0);
+}
+
+TEST_CASE("Full House gives 25 points"){
+    //Check 5 dice and see if 3 is one number and 2 is another
+}
+
+TEST_CASE("Small Straight gives 30 points"){
+    //Check 3 dice to see if they are the same number
+}
+
+TEST_CASE("Large Straight gives 40 points"){
+    //Check 4 dice to see if they are the same number
+}
+
+TEST_CASE("Yahtzee gives 50 points"){
+    //Check 5 dice to see if they are the same number
+}
+
+TEST_CASE("Scoreboard puts each player in order"){
+    //Test 2 final scores and see if one is above the other on scoreboard
+}
+
+TEST_CASE("Score resets after each game"){
+    //Make sure that after a game is played that score is reset
+}
+
+TEST_CASE("Player can decide whether to continue playing the game or not"){
+    //Test that Yes/No works
+}
