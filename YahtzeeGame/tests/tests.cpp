a534581516dfca6a56b1ddd90ea1bd0b82b44ae6
xgschwi <xgschwi@bgsu.edu>
2020-10-25 19:48:16 -0400

    Fixed up test cases, and yml changed to use make commands

--- YahtzeeGame/tests/tests.cpp
+++ YahtzeeGame/tests/tests.cpp
@@ -21,62 +21,58 @@ TEST_CASE("Dice can be rolled", "[dice]") {
 }
 
 TEST_CASE("Score starts at 0", "[score]"){
-    Scorecard score1;
+    ScoreCard score1;
 
     REQUIRE(score1.score == 0);
 }
 
-TEST_CASE("Full House gives 25 points", "[score]"){
-    Dice dice;
-    dice.setvalues(1,1,1,2,2);
-
-    REQUIRE(score >= 25);
+TEST_CASE("Full House gives 25 points", "[fullHouseF]"){
+    ScoreCard scoreC;
+    REQUIRE(scoreC.fullHouseF() == 25);
 }
 
-TEST_CASE("Small Straight gives 30 points", "[score]"){
-    Dice dice;
-    dice.setvalues(1,2,3,4,3);
-
-    REQUIRE(score >= 30);
+TEST_CASE("Small Straight gives 30 points", "[smallF]"){
+    ScoreCard scoreC;
+    REQUIRE(scoreC.smallF() == 30);
 }
 
-TEST_CASE("Large Straight gives 40 points", "[score]"){
-    Dice dice;
-    dice.setvalues(1,2,3,4,5);
+TEST_CASE("Large Straight gives 40 points", "[largeF]"){
+     ScoreCard scoreC;
 
-    REQUIRE(score >= 40);
+    REQUIRE(scoreC.largeF() == 40);
 }
 
-TEST_CASE("Yahtzee gives 50 points", "[score]"){
-    Dice dice;
-    dice.setvalues(1,1,1,1,1);
-
-    REQUIRE(score >= 50);
+TEST_CASE("Yahtzee gives 50 points", "[yahtzeeF]"){
+     ScoreCard scoreC;
+    REQUIRE(scoreC.yahtzeeF() == 50);
 }
 
-TEST_CASE("Three of a kind checks sum", "[score]"){
-    Dice dice;
-    dice.setvalues(1,1,1,3,4);
+TEST_CASE("Three of a kind checks sum", "[threeKindF]"){
+     ScoreCard scoreC;
+    int a[5] = {1,1,1,3,4};
     int sum = 0;
-    int *a = dice.rollDice();
     for(int i = 0; i < 5; i++) sum += a[i];
 
     REQUIRE(sum == 10);
+    REQUIRE(scoreC.threeKindF(a) == 10 );
 }
 
-TEST_CASE("Four of a kind checks sum", "[score]"){
-    Dice dice;
-    dice.setvalues(1,1,1,1,4);
+TEST_CASE("Four of a kind checks sum", "[fourKindF]"){
+    ScoreCard scoreC;
+    int a[5] = {1,1,1,1,4};
     int sum = 0;
-    int *a = dice.rollDice();
     for(int i = 0; i < 5; i++) sum += a[i];
 
-    REQUIRE(sum == 9);
+    REQUIRE(sum == 8);
+    REQUIRE(scoreC.fourKindF(a) == 8);
 }
 
-TEST_CASE("Score resets after each game"){
-    games = 1;
-    Scorecard score1;
+TEST_CASE("Chance gives a sum of all numbers", "[chanceF]"){
+    int a[5] = {1,1,1,1,1};
+    int sum = 0;
+    for(int i = 0; i < 5; i++) sum += a[i];
 
-    REQUIRE(score1.score == 0);
+    ScoreCard scoreC;
+    REQUIRE(sum == 5);
+    REQUIRE(scoreC.chanceF(a) == 5);
 }
